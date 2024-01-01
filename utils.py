d65d4d51dbaf2fef1759b6fba0461cc638e038a7
Drake Thomas <grahamsnumberisbig@gmail.com>
2024-01-01 11:05:39 -0800

    not a set anymore

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def complete(t1,t2):
     return tuple((-(np.array(t1,dtype=int)+np.array(t2,dtype=int)))%3)
 def greedy_solve(n,f):
     #given a function f from n-tuples to R, generate a greedy solution
-    candidates = set(itertools.product([0,1,2],repeat=n))
+    candidates = itertools.product([0,1,2],repeat=n)
     scored_candidates = [(c,f(c)) for c in candidates]
     scored_candidates = sorted(scored_candidates, key = lambda x: -x[1])
     value_dict = {c: v for c,v in scored_candidates}
